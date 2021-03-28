da5295e5d6f5074673af183081ef5d37c6421699
Sergio Vaquero Garcia <svg26@msn.com>
2021-03-28 12:41:46 +0200

    VERSIÓN INICIAL. ARRANCAMOS!!!!!

---
+++ sumador/test/sumador/TestASumar.java
@@ -0,0 +1,14 @@
+package sumador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestASumar {
+
+	@Test
+	public final void test() {
+		fail("No implementado aun"); // TODO
+	}
+
+}
