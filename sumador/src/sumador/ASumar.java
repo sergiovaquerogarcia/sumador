854a1fa9508bed653f2caa01ea32eb4fc8bbddd0
Sergio Vaquero Garcia <svg26@msn.com>
2021-03-28 12:58:36 +0200

    REALIZAMOS EL PRIMER TEST DE PRUEBA

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,5 +1,35 @@
 package sumador;
 
+/**
+ * CLASE para SUMAR los DÍGITOS que componen un NÚMERO.
+ * @author Sergio Vaquero
+ * @version 1.0
+ *
+ */
 public class ASumar {
+	
+	private String numero;
+
+	/**
+	 * CONSTRUCTOR de la CLASE con parámetro.
+	 * @param numero. Número a Sumar
+	 */
+	public ASumar(String numero) {
+		this.numero = numero;
+	}
+
+	/**
+	 * MÉTODO para MOSTRAR el RESULTADO de la SUMA de todos los dígitos de un número.
+	 * @param num. Cadena que contiene el número a sumar.
+	 * @return devuelve una Cadena donde muestra en número introducido y la suma de todos sus dígitos.
+	 */
+	public String mostrar(String num) {
+		String aux = num;
+		
+		if (aux.length() == 1) {
+			return (aux + " = " + aux);
+		}
+		else return null;
+	}
 
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -4,11 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * CLASE JUNIT para realizar los Test de Prueba
+ * @author Sergio Vaquero
+ * @version 1.0
+ *
+ */
 public class TestASumar {
 
+	/**
+	 *  TEST para comprobar un número de 1 sólo dígito 
+	 */
 	@Test
-	public final void test() {
-		fail("No implementado aun"); // TODO
+	public void testUnaCifra() {
+		 ASumar numero = new ASumar("4");
+	     String resultado = numero.mostrar("4");
+	     assertEquals("4 = 4",resultado );
 	}
 
 }
